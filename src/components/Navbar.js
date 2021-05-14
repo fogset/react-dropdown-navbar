a76e1ccea1da6c19410e0c7f8ceede49cb8d4389
fogset <zhangth1990@hotmail.com>
2021-05-14 10:26:24 -0700

    added button and button css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,10 @@ import Dropdown from './Dropdown'
 
 function Navbar(){
     const [click, setClick] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu =() => setClick(false);
     return(
         <>
             <nav className='navbar'>
@@ -18,6 +20,30 @@ function Navbar(){
                 <div className='menu-icon' onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services <i className='fas fa-caret-down' />
+                        </Link>
+                        {dropdown && <Dropdown />}
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/contact-us' className='nav-links' onClick={closeMobileMenu}>
+                            Contact Us
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
+                <Button/>
             </nav>
         </>
     )
